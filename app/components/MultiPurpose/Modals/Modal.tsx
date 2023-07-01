7a728401fbde7a95048bf67266c7bdc5acb27b03
Keyzzzt <tallinnn@hotmail.com>
2023-07-01 17:41:52 +0300

    Finished register modal UI

--- app/components/MultiPurpose/Modals/Modal.tsx
+++ app/components/MultiPurpose/Modals/Modal.tsx
@@ -1,97 +1,105 @@
-'use client'
-import { FC, useEffect, useState, useCallback } from 'react'
-import { ModalProps } from "./ModalProps"
-import { styles } from './ModalStyles'
-import { IoMdClose } from 'react-icons/io'
-import { Button } from '../Button/Button'
+"use client";
+import { FC, useEffect, useState, useCallback } from "react";
+import { styles } from "./ModalStyles";
+import { IoMdClose } from "react-icons/io";
+import { Button } from "../Button/Button";
+
+type ModalProps = {
+  isOpen?: boolean;
+  title?: string;
+  actionLabel: string;
+  secondaryActionLabel?: string;
+  disabled?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  secondaryAction?: () => void;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+};
 
 export const Modal: FC<ModalProps> = ({
-    isOpen,
-    disabled,
-    onClose,
-    onSubmit,
-    secondaryAction,
-    title,
-    body,
-    actionLabel,
-    secondaryActionLabel
+  isOpen,
+  disabled,
+  onClose,
+  onSubmit,
+  secondaryAction,
+  title,
+  body,
+  actionLabel,
+  secondaryActionLabel,
+  footer,
 }) => {
-    const [showModal, setShowModal] = useState(isOpen)
+  const [showModal, setShowModal] = useState(isOpen);
 
-    const handleClose = () => useCallback(() => {
-        if (!disabled) {
-            setShowModal(false)
-            setTimeout(() => {
-                onClose()
-            }, 300)
-        }
-    }, [disabled, onClose])
-
-    const handleSubmit = () => useCallback(() => {
-        if (!disabled) {
-            onSubmit()
-        }
-    }, [disabled, onSubmit])
-
-    const handleSecondaryAction = () => useCallback(() => {
-        if (!disabled && secondaryAction) {
-            secondaryAction()
-        }
-    }, [disabled, secondaryAction])
-
-    useEffect(() => {
-        setShowModal(isOpen)
-    }, [isOpen])
-
-    if (!isOpen) {
-        return null
+  const handleClose = useCallback(() => {
+    if (!disabled) {
+      setShowModal(false);
+      setTimeout(() => {
+        onClose();
+      }, 300);
     }
+  }, [disabled, onClose]);
 
-    const concatenatedStyles = `${styles.modalContent} ${showModal
-        ? 'translate-y-0'
-        : 'translate-y-full'} ${showModal
-            ? ' opacity-100'
-            : ' opacity-0'}`
+  const handleSubmit = useCallback(() => {
+    if (!disabled) {
+      onSubmit();
+    }
+  }, [disabled, onSubmit]);
 
+  const handleSecondaryAction = useCallback(() => {
+    if (!disabled && secondaryAction) {
+      secondaryAction();
+    }
+  }, [disabled, secondaryAction]);
 
+  useEffect(() => {
+    setShowModal(isOpen);
+  }, [isOpen]);
 
-    return (
-        <>
-            <div className={styles.modal}>
-                <div className={styles.modalWrapper}>
-                    {/* CONTENT */}
-                    <div className={concatenatedStyles}>
-                        <div className={styles.any}>
-                            <header className={styles.modalHeader}>
-                                <button onClick={handleClose} className={styles.closeBtn}>
-                                    <IoMdClose size={18} />
-                                </button>
-                                <div className={styles.modalTitle}>{title}</div>
-                            </header>
-                            <div className={styles.modalBody}>{body}</div>
-                            <footer className={styles.modalFooter}>
-                                <div className={styles.modalFooterBody}>
-                                    {secondaryAction && secondaryActionLabel && (
-                                        <Button
-                                            outline
-                                            disabled={disabled}
-                                            label={secondaryActionLabel}
-                                            onClick={handleSecondaryAction}
-                                        />
-                                    )}
-                                    <Button
-                                        disabled={disabled}
-                                        label={actionLabel}
-                                        onClick={handleSubmit}
-                                    />
-                                </div>
-                            </footer>
-                        </div>
-                    </div>
+  if (!isOpen) {
+    return null;
+  }
+
+  const concatenatedStyles = `${styles.modalContent} ${
+    showModal ? "translate-y-0" : "translate-y-full"
+  } ${showModal ? " opacity-100" : " opacity-0"}`;
+
+  return (
+    <>
+      <div className={styles.modal}>
+        <div className={styles.modalWrapper}>
+          {/* CONTENT */}
+          <div className={concatenatedStyles}>
+            <div className={styles.any}>
+              <header className={styles.modalHeader}>
+                <button onClick={handleClose} className={styles.closeBtn}>
+                  <IoMdClose size={18} />
+                </button>
+                <div className={styles.modalTitle}>{title}</div>
+              </header>
+              <div className={styles.modalBody}>{body}</div>
+              <footer className={styles.modalFooter}>
+                <div className={styles.modalFooterBody}>
+                  {secondaryAction && secondaryActionLabel && (
+                    <Button
+                      outline
+                      disabled={disabled}
+                      label={secondaryActionLabel}
+                      onClick={handleSecondaryAction}
+                    />
+                  )}
+                  <Button
+                    disabled={disabled}
+                    label={actionLabel}
+                    onClick={handleSubmit}
+                  />
                 </div>
+                {footer}
+              </footer>
             </div>
-
-        </>
-    )
-}
-
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,29 @@
-import { RegisterModal } from './components/MultiPurpose/Modals/RegisterModal/RegisterModal'
-import { Navbar } from './components/Navbar/Navbar'
-import './globals.css'
-import { Nunito } from 'next/font/google'
+import { RegisterModal } from "./components/MultiPurpose/Modals/RegisterModal/RegisterModal";
+import { Navbar } from "./components/Navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import { ToasterProvider } from "./providers/ToasterProvider";
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Markee',
-  description: 'New life to start...',
-}
+  title: "Markee",
+  description: "New life to start...",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <RegisterModal  />
-        <Navbar/>
-        {children}</body>
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
