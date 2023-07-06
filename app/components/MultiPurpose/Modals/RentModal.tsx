601b44c2cb2b6d7de79da58e4c4d10f3a73cfce6
Keyzzzt <tallinnn@hotmail.com>
2023-07-06 12:51:27 +0300

    Rent modal step 2

--- app/components/MultiPurpose/Modals/RentModal.tsx
+++ app/components/MultiPurpose/Modals/RentModal.tsx
@@ -1,21 +1,14 @@
 "use client";
 import React from "react";
-import axios from "axios";
-import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
-import { useRegisterModal } from "@/app/helpers/hooks/useRegisterModal";
-import { useLoginModal } from "@/app/helpers/hooks/useLoginModal";
+import { useForm, FieldValues } from "react-hook-form";
 import { Modal } from "./Modal";
 import { Heading } from "../Heading";
-import { Input } from "../Inputs/Input";
-import { toast } from "react-hot-toast";
-import { Button } from "../Button/Button";
-import { FcGoogle } from "react-icons/fc";
-import { AiFillGithub } from "react-icons/ai";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { useRentModal } from "@/app/helpers/hooks/useRentModal";
 import { categories } from "../../Navbar/Categories";
 import { CategoryInput } from "../Inputs/CategoryInput";
+import { CountrySelect } from "../Inputs/CountrySelect";
+import dynamic from "next/dynamic";
+import { Counter } from "../Inputs/Counter";
 
 enum Steps {
   Category = 0,
@@ -71,6 +64,18 @@ export const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
+
+  const Map = React.useMemo(
+    () =>
+      dynamic(() => import("../../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -102,7 +107,50 @@ export const RentModal = () => {
   );
 
   if (step === Steps.Location) {
-    body = <div>Location</div>;
+    body = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located"
+          subTitle="Help guests to find you!"
+        />
+        <CountrySelect
+          onChange={(value) => setCustomValue("location", value)}
+          value={location}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
+  if (step === Steps.Info) {
+    body = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subTitle="What amenities do you have?"
+        />
+        <Counter
+          title="Guests"
+          subTitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue("guestCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subTitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => setCustomValue("roomCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subTitle="How many bathrooms do you have?"
+          value={bathroomCount}
+          onChange={(value) => setCustomValue("bathroomCount", value)}
+        />
+      </div>
+    );
   }
 
   return (
